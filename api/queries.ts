0ba4494bd4dc8aaab3afc35e447c469827a77131
spences10 <spences10apps@gmail.com>
2020-07-11 14:02:27 +0100

    add query strings

---
+++ api/queries.ts
@@ -0,0 +1,61 @@
+export const GET_LINKS = `
+  query {
+    allLinks {
+      data {
+        name
+        _id
+        url
+        description
+        archived
+      }
+    }
+  }
+`
+
+export const CREATE_LINK = `
+  mutation($name: String!, $url: String!, $description: String!) {
+    createLink(
+      data: { name: $name, url: $url, description: $description, archived: false }
+    ) {
+      name
+      _id
+      url
+      description
+      archived
+    }
+  }
+`
+
+export const UPDATE_LINK = `
+  mutation(
+    $id: ID!
+    $archived: Boolean!
+    $name: String!
+    $url: String!
+    $description: String!
+  ) {
+    updateLink(
+      id: $id
+      data: {
+        name: $name
+        url: $url
+        description: $description
+        archived: $archived
+      }
+    ) {
+      name
+      _id
+      url
+      description
+      archived
+    }
+  }
+`
+
+export const DELETE_LINK = `
+  mutation($id: ID!) {
+    deleteLink(id: $id) {
+      _id
+    }
+  }
+`
