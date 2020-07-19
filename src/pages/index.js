24074305d41de92e9df774364c7c2767c6e40628
spences10 <spences10apps@gmail.com>
2020-07-19 18:50:48 +0100

    working form no refresh

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,41 +2,24 @@ import React from 'react'
 import { useQuery } from 'react-query'
 import { ReactQueryDevtools } from 'react-query-devtools'
 import Layout from '../components/layout'
-
-const Dump = props => (
-  <div
-    style={{
-      fontSize: 20,
-      border: '1px solid #efefef',
-      padding: 10,
-      background: 'white',
-    }}
-  >
-    {Object.entries(props).map(([key, val]) => (
-      <pre key={key}>
-        <strong style={{ color: 'white', background: 'red' }}>
-          {key} 💩
-        </strong>
-        {JSON.stringify(val, '', ' ')}
-      </pre>
-    ))}
-  </div>
-)
+import LinkForm from '../components/link-form'
 
 export default () => {
   const fetchData = async () => {
-    let name = `Linky Link`
-    let url = `https://sasaljjdfks.com`
-    let description = `Linky Link Description`
-    const body = { name, url, description }
+    // let name = `Linky Link`
+    // let url = `https://sasaljjdfks.com`
+    // let description = `Linky Link Description`
+    // const body = { name, url, description }
     // const response = await fetch(`/api/create-link`, {
     //   method: `POST`,
     //   body: JSON.stringify(body),
     // })
-    const response = await fetch(`/api/get-all-links`)
+    // const response = await fetch(`/api/get-all-links`)
     // const response = await fetch(
     //   `/api/get-links?queryName=${READ_LINKS}&queryMethod=POST`
     // )
+
+    const response = await fetch(`/api/get-all-links`)
     const data = await response.json()
     return data
   }
@@ -61,16 +44,14 @@ export default () => {
       <ReactQueryDevtools initialIsOpen={false} />
       <h1>Hello</h1>
       {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <LinkForm refreshLinks={fetchData} />
       {linksData.map(l => {
-        console.log('=====================')
-        console.log(l)
-        console.log('=====================')
         return (
-          <>
+          <li key={l._id}>
             <p>{l.name}</p>
             <p>{l.url}</p>
             <p>{l.description}</p>
-          </>
+          </li>
         )
       })}
     </Layout>
